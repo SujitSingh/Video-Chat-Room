0baadde898a71775c48d68082350824beece4110
Sujit Kumar Singh <sujit98322@gmail.com>
2020-12-31 20:24:51 +0530

    Adding Peer server for generating user ID

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,11 @@
 const socket = io('/');
+const peer = new Peer(undefined, {
+  host: 'localhost', port: 3080, path: '/peerjs/peer'
+});
 
-socket.emit('room-joined', roomId, 10); // inform others about room joining
+peer.on('open', id => {
+  socket.emit('room-joined', roomId, id); // inform others about room joining
+});
 
 socket.on('user-joined', userId => {
   // listen for new user's joining
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const http = require('http');
 const { v4: uuid4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
 
 const app = express();
-const server = http.Server(app);
+const server = http.createServer(app);
 const io = require('socket.io')(server);
-const PORT = 3080;
+const PORT = process.env.PORT ||3080;
 
+app.enable('trust proxy');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
@@ -18,6 +20,12 @@ app.get('/:roomId', (req, res) => {
   res.render('room', { roomId: req.params.roomId });
 });
 
+// registering peer-server route
+const peerServer = ExpressPeerServer(server, {
+  path: '/peer' // host/peerjs/peer
+});
+app.use('/peerjs', peerServer);
+
 io.on('connection', socket => {
   socket.on('room-joined', (roomId, userId) => {
     socket.join(roomId);
@@ -26,7 +34,7 @@ io.on('connection', socket => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running at http:127.0.0.1:${PORT}`);
+  console.log(`Server running at http://127.0.0.1:${PORT}`);
 }).on('error', err => {
   console.log('Server starting error -', err);
 });
